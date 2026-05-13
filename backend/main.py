5ecd648e42d1a29ee00707917bf7009282366cd2
James Wang <james.wang375@berkeley.edu>
2026-05-12 20:45:11 -0700

    fix CORS credentials

--- backend/main.py
+++ backend/main.py
@@ -38,6 +38,7 @@ origins = os.getenv("ALLOWED_ORIGINS", "http://localhost:5173").split(",")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
